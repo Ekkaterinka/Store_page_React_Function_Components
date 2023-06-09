45d9eaba077850322ac391e6756fb000728cc1ca
Ekaterina <120379585+Ekkaterinka@users.noreply.github.com>
2023-06-09 08:56:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://Ekkaterinka.github.io/Zadanie_01/',
+  base: '/Zadanie_01/',
   plugins: [react()],
 })
